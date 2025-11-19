3d009c758969046b4622cdfc5a29cfc2df0747ae
braieze <82060158+braieze@users.noreply.github.com>
2025-11-18 23:52:02 -0300

    Create firebase.js

---
+++ js/firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-firestore.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyC_gewonkD_XnzQmwmUnQuhfzOf5zdm9lw",
+    authDomain: "calculadora-6x2.firebaseapp.com",
+    projectId: "calculadora-6x2",
+    storageBucket: "calculadora-6x2.firebasestorage.app",
+    messagingSenderId: "238926197886",
+    appId: "1:238926197886:web:d05857b007dcb305e9d42f"
+};
+
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const appId = 'default-app-id'; // O tu ID de entorno
